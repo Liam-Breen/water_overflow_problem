72807440623a976d878ccc60682d77b149e1a7f2
Liam-Breen <liam.breen25@gmail.com>
2021-02-14 23:25:53 +1100

    added some test data

--- main.py
+++ main.py
@@ -8,10 +8,42 @@ class Glass:
         self.capacity = 0.250
         self.water = 0
 
+    def fill(self, water: float):
+        ...
+
 class TreeSearch:
 
-    def __init__(self, i, j, water):
+    def __init__(self, i: int, j: int, water: float):
         self.row = i
         self.col = j
         self.water = water
         self.root = Glass()
+
+    def find_glass(self):
+        ...
+        # return glass
+
+
+if __name__ == "__main__":
+
+    test_values = [[0, 0, 0], [1, 1, 0], [1, 1, 2], [3, 2, 4]]
+    expected_outputs = [0, 0, 0.250, 0.250]
+
+    for index, test_value in enumerate(test_values):
+
+        i = test_values[0]
+        j = test_values[1]
+        water = test_values[2]
+
+        tree = TreeSearch(i, j, water)
+        water_level = tree.find_glass()
+
+        try:
+            assert water_level == expected_outputs[index], f"The water level was {water_level} when it should have been {expected_outputs[index]}"
+
+        except AssertionError as error:
+            print(error)
+
+
+
+
